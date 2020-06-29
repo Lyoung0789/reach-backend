9db1af9e27bdb11da484f685e6eb4bc4c4b42b14
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-29 15:24:45 -0400

    Adding Table data for Migration

---
+++ db/migrate/20200629192123_create_todos.rb
@@ -0,0 +1,14 @@
+class CreateTodos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :todos do |t|
+      t.string :title 
+      t.string :description 
+      t.string :image_url
+      t.string :category
+      t.boolean :completed
+
+
+      t.timestamps
+    end
+  end
+end
---
+++ notes.md
@@ -0,0 +1,17 @@
+# Goals
+has_many todos
+Title: string 
+description: String 
+image: string
+category: string 
+completed: boolean
+
+
+
+
+# Todos
+belongs_to: Goal 
+goal_id: integer 
+description: string 
+completed: boolean 
+
