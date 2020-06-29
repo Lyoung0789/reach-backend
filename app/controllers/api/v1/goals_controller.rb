f9fcb07484f8cbb48679ee6fab725dfc062801b9
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-29 17:24:20 -0400

    Adding Pry gem and index and show actions for Goals controller

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -1,19 +1,23 @@
 class Api::V1::GoalsController < ApplicationController
 
     def index
+        goals = Goal.all 
+        render json: goals
     end 
 
     def show
+        goal = Goal.all.find_by(id: params[:id])
+        render json: goal
     end 
 
-    def create
-    end 
+    # def create
+    # end 
 
-    def update
-    end 
+    # def update
+    # end 
 
-    def destroy
-    end 
+    # def destroy
+    # end 
 
 
     private 
