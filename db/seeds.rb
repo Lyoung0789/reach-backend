d4e1eef8bd800f358422ec4e486f12c58e5dee72
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-29 16:58:42 -0400

    Adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Goal.create(title: "Travel to Japan", description: "Travel to Tokyo! Its my dream!", category: "Travel")
+
+Todo.create(goal_id: 1, title: "Buy plane ticket", description: "I need to buy a ticket before I can even go lol")
