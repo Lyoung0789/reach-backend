e5596819dc4aeb862793f5a11f2c86a91e58a398
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-29 15:37:11 -0400

    adding validations to goal model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,6 @@
 class Goal < ApplicationRecord
+    has_many: todos
+    validates :title, presence: true
+    validates :category, presence: true
+    validates :category, inclusion: {in: ['Budget', 'Travel', 'Health', 'Mental Health', 'Spiritual'] }
 end
