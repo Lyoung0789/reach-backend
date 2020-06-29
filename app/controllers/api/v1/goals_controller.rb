5458698845256023a3d4674de5a6b3afbcdf4bf3
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-29 17:13:07 -0400

    Adding Controller actions to GoalsController

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::GoalsController < ApplicationController
+
+    def index
+    end 
+
+    def show
+    end 
+
+    def create
+    end 
+
+    def update
+    end 
+
+    def destroy
+    end 
+
+
+    private 
+    def goal_params
+        params.require(:goal).permit(:title, :category, :description, :completed, :imageurl)
+    end 
+
 end
