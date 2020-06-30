2175b4e82e3927f6aeb4c40214367dde15808ce9
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-30 00:05:48 -0400

    removing whitespace

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,5 @@ Goal.create(title: "Travel to Japan", description: "Travel to Tokyo! Its my drea
 
 Goal.create(title: "Make a Million Dollars", description: "I would like to make a million dollars before I die!", category: "Budget")
 
-
 Todo.create(goal_id: 1, title: "Buy plane ticket", description: "I need to buy a ticket before I can even go lol")
 
