f1c9b15925bf8fdf8a3496325ac0e2ad43636a2e
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-29 23:49:12 -0400

    Adding some more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 Goal.create(title: "Travel to Japan", description: "Travel to Tokyo! Its my dream!", category: "Travel")
 
+Goal.create(title: "Make a Million Dollars", description: "I would like to make a million dollars before I die!", category: "Budget")
+
+
 Todo.create(goal_id: 1, title: "Buy plane ticket", description: "I need to buy a ticket before I can even go lol")
+
